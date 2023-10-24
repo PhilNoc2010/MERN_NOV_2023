30ff0dbb27cb451438e0d805480d3dfc81fb692f
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-10-24 15:22:03 -0600

    add w1d2 functional .map .fitler

---
+++ W1D2/functional/callback-map-filter.js
@@ -0,0 +1,88 @@
+// Functional Programming
+
+const myCoolCallback = () => {
+    console.log("start");
+}
+
+setTimeout(myCoolCallback, 3000);
+
+// console.log("end");
+
+const animals = ["leopard", "giraffe", "zebra", "elephant", "monkey", "lion"];
+
+const newAnimals = animals.map((animal, i) => {
+    return `${i + 1} - ${animal} is cool`;
+});
+
+// console.log(newAnimals)
+// console.log(animals)
+
+// ------- filter -------
+const filteredAnimals = animals.filter((animal) => {
+    return animal.length <= 5;
+});
+
+// console.log(filteredAnimals);
+// -------------
+const people = Object.freeze([
+    { name: "Bob", age: 30 },
+    { name: "Mary", age: 31 },
+    { name: "Sue", age: 28 },
+    { name: "George", age: 38 }
+]);
+
+const newPeople = people.map((person) => {
+    return `${person.name} is ${person.age} years old`;
+});
+// console.log(newPeople);
+
+// filter people with age greater than or equal to 30
+const filteredPeople = people.filter((eachElement) => {
+    return eachElement.age >= 30;
+});
+
+console.log(filteredPeople);
+filteredPeople[0].name = "unicorn";
+console.log(filteredPeople);
+console.log(people);
+
+const pokemon = Object.freeze([
+    { "id": 1, "name": "Bulbasaur", "types": ["poison", "grass"] },
+    { "id": 5, "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9, "name": "Blastoise", "types": ["water"] },
+    { "id": 12, "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16, "name": "Pidgey", "types": ["normal", "flying"] },
+    { "id": 23, "name": "Ekans", "types": ["poison"] },
+    { "id": 24, "name": "Arbok", "types": ["poison"] },
+    { "id": 25, "name": "Pikachu", "types": ["electric"] },
+    { "id": 37, "name": "Vulpix", "types": ["fire"] },
+    { "id": 52, "name": "Meowth", "types": ["normal"] },
+    { "id": 63, "name": "Abra", "types": ["psychic"] },
+    { "id": 67, "name": "Machamp", "types": ["fighting"] },
+    { "id": 72, "name": "Tentacool", "types": ["water", "poison"] },
+    { "id": 74, "name": "Geodude", "types": ["rock", "ground"] },
+    { "id": 87, "name": "Dewgong", "types": ["water", "ice"] },
+    { "id": 98, "name": "Krabby", "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime", "types": ["psychic"] },
+    { "id": 133, "name": "Eevee", "types": ["normal"] },
+    { "id": 144, "name": "Articuno", "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos", "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres", "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair", "types": ["dragon"] }
+]);
+
+const bListPkmn = pokemon.filter(p => p.name[0] === "B");
+// console.log(bListPkmn);
+
+// an array containing just the first type of all the pokémon whose second type is "flying"
+const newPoke = pokemon.filter((poke) => {
+    return poke.types[1] === "flying"
+}).map( poke => {
+    return `${poke.name}'s first type is ${poke.types[0]}`
+} )
+
+console.log(newPoke)
+
+// const newPoke2 = pokemon.filter(poke => poke.types[1] === "flying")
+//     .map( poke =>`${poke.name}'s first type is ${poke.types[0]}`)
