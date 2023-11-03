df00fba5cb67f5cdfafb88ed09f7a2ae03579a23
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-11-03 11:21:55 -0600

    add algos for w3d2 w3d1

---
+++ ALGOS/w3d2_entries_insert/nov2023.js
@@ -0,0 +1,30 @@
+// recreate Object.entries
+
+const obj1 = {
+    food: "Pizza",
+    calories: 9001,
+};
+
+const obj2 = {
+    firstName: "Foo",
+    lastName: "Bar",
+    power: 9001,
+};
+
+// linking objects
+obj1.__proto__ = obj2;
+
+for (let someKey in obj1) {
+    console.log(someKey, obj1[someKey]);
+}
+
+// returns an array of keys
+const keyArray = Object.keys(obj2);
+// console.log(keyArray)
+
+const valuesArray = Object.values(obj2);
+// console.log(valuesArray)
+
+console.log("-----------")
+const entries = Object.entries(obj1);
+console.log(entries)
